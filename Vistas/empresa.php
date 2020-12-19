6a5fe87d7bd66afcfe9e453028856a374581beaf
Nico <celerinicolas@gmail.com>
2020-12-19 09:35:59 -0300

    la empresa completo

--- Vistas/empresa.php
+++ Vistas/empresa.php
@@ -4,15 +4,16 @@
 
 		<div class="container-fluid  m-superior-empresa d-flex" name="empresa">	
 			<div class="row align-content-center">				
-				<div class="col-12 mt-5  alto-empresa">
-					<img class=" img-fluid d-none d-md-block" src="../img/1x/Laempresa.jpg" alt="">
+				<div class="col-12 p-0">
+					<img class=" img-fluid d-none d-md-block img-empresa" src="../img/1x/Laempresa.jpg" alt="">
 					<img class=" img-fluid d-block d-md-none" src="../img/1x/LaEmpresaMobile.jpg" alt="">					
 				</div>
-				<div class="col-md-8 offset-md-2 mt-5 alto">				
-					<h2 >La Empresa</h2>
-					<p class="text-justify texto-empresa lead p-2">Credifiar es una empresa dedicada a la prestación de servicios financieros que hace más de 10 años se destaca en el mercado santafesino ofreciendo a sus clientes la mejor financiación. Con más de 2000 comercios adheridos tanto en la ciudad como en la región y más de 50.000 clientes, Credifiar trabaja junto a los comercios generando promociones, descuentos y planes de cuotas para brindarles a los clientes la posibilidad de comprar en forma más cómoda y accesible.
-					</p>
-					<p class="font-weight-bold">Credifiar, una empresa que confía en vos.</p>	
+				<div class="d-md-flex alto-empresa ">
+					<div class="col-md-8 offset-md-2 mt-5 align-content-center justify-content-center ">			
+						<h2 >La Empresa</h2>
+						<p class="text-justify texto-empresa lead p-2">Credifiar es una empresa dedicada a la prestación de servicios financieros que hace más de 10 años se destaca en el mercado santafesino ofreciendo a sus clientes la mejor financiación. Con más de 2000 comercios adheridos tanto en la ciudad como en la región y más de 50.000 clientes, Credifiar trabaja junto a los comercios generando promociones, descuentos y planes de cuotas para brindarles a los clientes la posibilidad de comprar en forma más cómoda y accesible.</p>
+						<p class="font-weight-bold">Credifiar, una empresa que confía en vos.</p>	
+					</div>
 				</div>
 			</div>
 		</div>
